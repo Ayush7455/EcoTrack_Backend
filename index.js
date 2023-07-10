5d382c8b169dfdd5949384575e42c379b8379ca2
Ayush757 <ayushhh.a757@gmail.com>
2023-07-10 22:43:47 +0530

    add user

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const bodyParser = require('body-parser');
 require('./models/User');
 const addUser=require("./routes/addUser")
 const getHandPrint=require("./routes/getHandPrint")
+const increaseHandPrint=require("./routes/increaseHandPrint")
 require('./db');
 app.use(bodyParser.json());
 app.use(addUser)
 app.use(getHandPrint)
+app.use(increaseHandPrint)
 app.listen(port, () => {
     console.log("Server is running on port " + port);
 })
