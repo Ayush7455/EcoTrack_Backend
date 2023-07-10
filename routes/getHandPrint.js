0466e4a818e013967bfc9653ec4cb1e77d8b8b88
Ayush757 <ayushhh.a757@gmail.com>
2023-07-10 23:09:22 +0530

    add user

--- routes/getHandPrint.js
+++ routes/getHandPrint.js
@@ -1,27 +1,31 @@
-const express=require("express")
-const router=express.Router();
-const mongoose=require("mongoose")
-const User=mongoose.model("User")
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
 
-require("dotenv").config()
+require("dotenv").config();
+
 router.post("/getHandPrint", (req, res) => {
-    const { email } = req.body;
-    if (!email) {
-      return res.status(404).json({ message: "Email is missing" });
-    } else {
-      User.findOne({ email: email })
-        .then((user) => {
-          if (!user) {
-            return res.status(404).json({ message: "User not found" });
-          }
-          const Handprint = user.handprint;
-          return res.status(200).json({ Handprint: Handprint });
-        })
-        .catch((err) => {
-          console.log(err);
-          return res.status(404).json({ message: "Internal server error" });
-        });
-    }
-  });
-  module.exports=router
-  
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(404).json({ message: "Email is missing" });
+  }
+
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      const handprint = user.handprint; // Retrieve the handprint array from the user
+
+      return res.status(200).json({ handprint: handprint }); // Return the handprint array
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({ message: "Internal server error" }); // Update the status code for the internal server error
+    });
+});
+
+module.exports = router;
