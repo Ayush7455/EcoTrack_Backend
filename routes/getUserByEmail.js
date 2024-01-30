918df285fd17f5ee9cad1b5b527111fecae5cde8
Ayush757 <ayushhh.a757@gmail.com>
2024-01-30 16:26:36 +0530

    Update

--- routes/getUserByEmail.js
+++ routes/getUserByEmail.js
@@ -6,19 +6,24 @@ const User = mongoose.model("User");
 require("dotenv").config();
 
 router.get("/getUserByEmail", (req, res) => {
-  const { email } = req.query
+  const { email } = req.query;
 
   if (!email) {
     return res.status(400).json({ message: "Email is required as a query parameter" });
   }
 
-  User.findOne({ email: email }, { email: 1, image: 1})
-    .then((user) => {
-      if (!user) {
-        return res.status(404).json({ message: "User not found" });
+  const keywordRegex = new RegExp(email, 'i'); // Create a case-insensitive regular expression
+
+  User.find({ email: keywordRegex }, { email: 1, image: 1 })
+    .then((users) => {
+      if (!users || users.length === 0) {
+        return res.status(404).json({ message: "No users found" });
       }
 
-      res.status(200).json({ email: user.email, image: user.image });
+      // Extract relevant information for each user
+      const results = users.map((user) => ({ email: user.email, image: user.image }));
+
+      res.status(200).json(results);
     })
     .catch((err) => {
       console.error(err);
