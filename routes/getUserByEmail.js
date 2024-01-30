10885c815f951aa6853153628d87092a035ca227
Ayush757 <ayushhh.a757@gmail.com>
2024-01-30 18:08:35 +0530

    Update

--- routes/getUserByEmail.js
+++ routes/getUserByEmail.js
@@ -6,39 +6,32 @@ const User = mongoose.model("User");
 require("dotenv").config();
 
 router.get('/getUserByEmail', (req, res) => {
-  const { keyword } = req.body;
+  const { keyword } = req.query;  // Change this line to extract from query parameters
 
   if (!keyword) {
-    return res.status(422).json({ error: "Please search a username" });
-}
+    return res.status(422).json({ error: "Please search an email" });
+  }
 
+  User.find({ email: { $regex: keyword, $options: 'i' } })
+    .then(users => {
+      let data = [];
+      users.forEach(item => {
+        data.push({
+          email: item.email,
+          name: item.name,
+          image: item.image,
+        });
+      });
 
- 
-User.find({ email: { $regex: keyword, $options: 'i' } })
-.then(user => {
-    
-    let data = [];
-    user.map(item => {
-        data.push(
-            {
-                email:item.email,
-                name: item.name,
-                image:item.image,
-            }
-        )
-    })
-
-    
-    if (data.length == 0) {
+      if (data.length === 0) {
         return res.status(422).json({ error: "No User Found" });
-    }
-    res.status(200).send({ message: "User Found", user: data });
+      }
 
-})
-.catch(err => {
-    res.status(422).json({ error: "Server Error" });
-})
-})
-
+      res.status(200).send({ message: "User Found", user: data });
+    })
+    .catch(err => {
+      res.status(500).json({ error: "Server Error" });
+    });
+});
 
 module.exports = router;
