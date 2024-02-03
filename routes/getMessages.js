34881464443d1f251feadcf6660ca05f8cd9faf2
Ayush757 <ayushhh.a757@gmail.com>
2024-02-03 13:56:38 +0530

    Update

--- routes/getMessages.js
+++ routes/getMessages.js
@@ -4,23 +4,23 @@ const mongoose = require("mongoose");
 const User = mongoose.model("User");
 require("dotenv").config();
 
-router.get("/getMessages", async (req, res) => {
-  const { userEmail } = req.body;
-
-  try {
-    const user = await User.findOne({ email: userEmail });
-
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
+router.get("/getMessages/:userEmail", async (req, res) => {
+    const { userEmail } = req.params;
+  
+    try {
+      const user = await User.findOne({ email: userEmail });
+  
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+  
+      const messages = user.messages;
+  
+      res.json({ messages });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal Server Error" });
     }
-
-    const messages = user.messages;
-
-    res.json({ messages });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
+  });
+  
 module.exports = router;
